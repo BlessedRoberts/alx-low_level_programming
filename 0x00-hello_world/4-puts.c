ffe02738a661a7b43b8a2ac7a043b8b41c1d8a6e
BlessedRoberts <kardesh25@gmail.com>
2022-12-09 01:30:45 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- * main- entry
- * Return- Always 0
- */
+
 int main(void)
 {
 		puts("Programming is like building a multilingual puzzle\n");
-
-			return (0);
+		return (0);
 }
