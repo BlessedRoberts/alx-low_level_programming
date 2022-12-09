36cd07c6a48318c6b670c9d7efa4b4e7cf0fdcb9
BlessedRoberts <kardesh25@gmail.com>
2022-12-09 02:44:41 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
-/**
- *main - Entry point
- * Return: Always 0 
- * /
 int main(void)
 {
-	printf("size of a chart: %i byte(s)\n",sizeof(chart)); 
-	printf("size of a chart: %i byte(s)\n",sizeof(int));
-	printf("size of a chart: %i byte(s)\n",sizeof(long int));
-	printf("size of a chart: %i byte(s)\n",sizeof(long long int));
-	printf("size of a chart: %i byte(s)\n",sizeof(float));
-	return (0);
+		printf("Size of a char: %i byte(s)\n", sizeof(char));
+	       	printf("Size of an int: %i byte(s)\n", sizeof(int));
+		printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	       	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %lu byte(s)\n", sizeof(float));
+		return (0);
 }
