bf5e3b42c26a7ce547a821c8adac8aeb41aeb7ff
BlessedRoberts <kardesh25@gmail.com>
2022-12-11 10:24:53 -0800

    alphabet

---
+++ 3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - Print alphabet in lower and upper case.
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
