c2a5ad7d5aa818cd466d76378515dc17ba2e9858
BlessedRoberts <kardesh25@gmail.com>
2022-12-18 08:30:51 -0800

    sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the charcter to be checked
  * Retrun: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int c);
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
