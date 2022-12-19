67cc8f21b279d46cf93cf89b2015ed6b19938f0f
BlessedRoberts <kardesh25@gmail.com>
2022-12-19 13:43:29 -0800

    Declaration

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,8 @@ void puts2(char *str)
 	int longi = 0;
 	int t = 0;
 	char *y = str;
-	int 0;
-	while (*y != '\0')
+	int o;
+		while (*y != '\0')
 	{
 		y++;
 		longi++;
@@ -24,6 +24,5 @@ void puts2(char *str)
 		_putchar(str[o]);
 	}
 	}
-
 	_putchar('\n');
 }
